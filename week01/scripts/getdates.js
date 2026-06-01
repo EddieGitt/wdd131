972652deb2cd35cf8266554b162ea28ba79a9545
Ediomo Victor <ebongediomo@gmail.com>
2026-06-01 13:26:38 +0100

    Update

--- week01/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// Dynamically populate the current copyright year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Dynamically populate the last modified date
-document.getElementById("lastModified").textContent =
-  "Last Modification: " + document.lastModified;
